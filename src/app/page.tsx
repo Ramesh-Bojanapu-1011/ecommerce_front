56b0db1cf40fdf8e3fd4725c565b36983a1e1d0f
Ramesh Bojanapu <84425972+lovelyram0143@users.noreply.github.com>
2025-01-26 22:45:04 +0530

    refactor layout and page components; remove preloader functionality and clean up code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,14 @@
-'use client';
 import Footer from '@/components/Footer';
 import Headder from '@/components/Headder';
-import { useEffect, useState } from 'react';
 
 export default function Home() {
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    /**
-     * Preloader
-     */
-    const preloader = document.querySelector('#preloader');
-    if (preloader) {
-      window.addEventListener('load', () => {
-        //  preloader.remove();
-        setIsClient(true);
-      });
-    }
-  }, []);
-
   return (
     <>
-      {isClient ? (
-        <>
-          <Headder />
-          <Footer />
-        </>
-      ) : (
-        <>
-          <div id="preloader"></div>
-        </>
-      )}
+      {/* <Breadcrumbs /> */}
+      <div>
+        <Headder />
+        <Footer />
+      </div>
     </>
   );
 }

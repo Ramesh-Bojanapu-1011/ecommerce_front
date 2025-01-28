9ed3ee8b3ecccb61052d2d71b4183c9601b909cb
Ramesh Bojanapu <84425972+lovelyram0143@users.noreply.github.com>
2025-01-28 16:09:15 +0530

    refactor: remove unused screen breakpoints from Tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,19 +7,6 @@ export default {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    screens: {
-      sm: { max: '550px' },
-      md: { max: '1050px' },
-      smlg: { min: '1051px', max: '1290px' },
-      lg: { max: '1400px' },
-      xl: { max: '1500px' },
-      customsm: { max: '550px' },
-      custommd: { min: '551px', max: '1050px' },
-      customlg: { min: '1051px', max: '1290px' },
-      customxl: { min: '1291px', max: '1400px' },
-      custom2xl: { min: '1401px', max: '1500px' },
-      custom3xl: { min: '1501px' },
-    },
     extend: {
       colors: {
         background: 'var(--background)',

a699ca65f490bd5e11bc16af2190e47f15076106
Ramesh Bojanapu <84425972+Ramesh-Bojanapu-1011@users.noreply.github.com>
2025-03-18 18:26:54 +0530

    fix: add canonical link to layout for improved SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Document</title>
+        <link rel="canonical" href="https://ecommerce-front-sand-psi.vercel.app/" />
 
         <meta
           name="google-site-verification"
